dc6bb64e1d000e7c2629abac429c5ad685f0b701
Nuttarika <nut_786@hotmail.com>
2012-07-31 15:03:17 +0700

    New HelloWorld

---
+++ src/th/ac/nu/students/u54347552/compro/ch01/HelloWorld.java
@@ -0,0 +1,11 @@
+
+package th.ac.nu.students.u54347552.compro.ch01;
+
+public class HelloWorld {
+    public static void main(String[]agrs){
+        System.out.println("Hello,World");
+        
+    }
+        
+    
+}
