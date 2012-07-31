21c732918c7a576ff45d42a638b5d22ae19d7c20
Nuttarika <nut_786@hotmail.com>
2012-07-31 15:05:19 +0700

    เพิ่ม Programming is fun

--- src/th/ac/nu/students/u54347552/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54347552/compro/ch01/HelloWorld.java
@@ -4,6 +4,7 @@ package th.ac.nu.students.u54347552.compro.ch01;
 public class HelloWorld {
     public static void main(String[]agrs){
         System.out.println("Hello,World");
+        System.out.println("Programming is fun");
         
     }
         
